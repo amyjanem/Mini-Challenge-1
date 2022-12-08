beac98c86bda94a24cf3b89b1268db6b18e463b6
Amy Mallett <amymallett8@gmail.com>
2022-12-08 06:42:57 +0000

    setup file changed

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     name='celebrity_births_test_amy', ## This will be the name your package will be published with
     version='0.0.1', 
     description='Mock package that allows you to find celebrity by date of birth',
-    url='https://github.com/IvanYingX/project_structure_pypi.git', # Add the URL of your github repo if published 
+    url='https://github.com/amyjanem/Mini-Challenge-1', # Add the URL of your github repo if published 
                                                                    # in GitHub
-    author='Ivan Ying', # Your name
+    author='Amy Mallett', # Your name
     license='MIT',
     packages=find_packages(), # This one is important to explain. See the notebook for a detailed explanation
     install_requires=['requests', 'beautifulsoup4'], # For this project we are using two external libraries
